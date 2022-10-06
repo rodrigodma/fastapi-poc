7134855ac734bc879990c01cac2e587488cd3a9a
rodrigodma <rdma@cesar.org.br>
2022-10-06 10:35:57 -0300

    (feat): changing BaseModel to Base from sqlalchemy

--- src/models/circle.py
+++ src/models/circle.py
@@ -1,4 +1,9 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer
 
-class Circle(BaseModel):
-    radial: int
+from ..infra.db import Base
+
+class Circle(Base):
+    __tablename__ = 'circles'
+    id = Column(Integer, primary_key=True, index=True)
+    radial = Column(Integer, nullable=False)
