18d10507497d520c51368da9ea93e02f3a17378c
rodrigodma <rdma@cesar.org.br>
2022-10-06 10:42:16 -0300

    (refact): changing circle router

--- src/routes/circle.py
+++ src/routes/circle.py
@@ -1,22 +1,24 @@
 from typing import List
 from fastapi import APIRouter, HTTPException
-from models import circle
+from models.circle import Circle
 from controller.circle_controller import CircleController as controller
 
-router = APIRouter()
+router = APIRouter(
+    prefix = "/circles",
+    tags = ["Circle"],
+    responses={404: {"description": "Not found"}},
+)
 
-Circle = circle.Circle
-
-@router.put("/circle/", response_model=Circle, tags=["circle"])
+@router.put("/", response_model=Circle)
 async def new_circle(circle: Circle):
     return await controller.save(circle)
 
-@router.get("/circle/", response_model=List[Circle], tags=["circle"])
+@router.get("/", response_model=List[Circle])
 async def get_circles():
     circles = await controller.getAll()
     return circles
 
-@router.get("/circle/{id}", response_model=Circle, tags=["hugs"])
+@router.get("/{id}", response_model=Circle)
 async def get_circle_by_id(id: int):
     circle = await controller.find(id)
     if circle is None:
